f748eea86c93f51a82bde283da970afa02464881
Lucio Paiva <luciopaiva@gmail.com>
2019-06-18 12:25:48 +0100

    Formalize Torrent class instance members

--- torrent.js
+++ torrent.js
@@ -3,8 +3,22 @@ import { arrayBufferToUtf8, assert } from "./utils.js";
 
 export default class Torrent {
 
-    /** @type {String} */
+    /** @type {String} URL of the tracker for this torrent (when there's a single one) */
     announce = null;
+    /** @type {String[]} URLs of the trackers for this torrent (when there're multiple ones) */
+    announceList = null;
+    /** @type {Date} when was this torrent created */
+    creationDate = null;
+    /** @type {Number} when there's a single file in the torrent, this is its size in bytes */
+    length = null;
+    /** @type {String} when there's a single file in the torrent, this is its name */
+    name = null;
+    /** @type {Number} a torrent is made up of pieces and each piece has this size in bytes */
+    pieceLength = null;
+    /** @type {ArrayBuffer[]} list of SHA-1 hashes for every piece of this torrent */
+    pieces = null;
+    /** @type {{name: String, length: Number}[]} list of files in the torrent (when there's more than one) */
+    files = null;
 
     /**
      * @param {Object} dictionary
